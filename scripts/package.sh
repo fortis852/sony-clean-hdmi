ef13e9a864ba5a63c183aec26d29d450c46822e5
fortis852 <98534716+fortis852@users.noreply.github.com>
2025-08-08 09:54:49 +0300

    Create package.sh

---
+++ scripts/package.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Package script to create APK
+
+echo "Creating APK package..."
+
+# Compile resources
+echo "Compiling resources..."
+# TODO: Add aapt commands
+
+# Create DEX files
+echo "Creating DEX files..."
+# TODO: Add dx commands
+
+# Package APK
+echo "Packaging APK..."
+# TODO: Add apkbuilder commands
+
+# Sign APK
+echo "Signing APK..."
+# TODO: Add jarsigner commands
+
+echo "✅ APK created: build/CleanHDMI.apk"

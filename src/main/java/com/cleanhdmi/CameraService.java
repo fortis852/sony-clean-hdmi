fcb15eb420a2e802581466e1442ff3ba2b7da42a
fortis852 <98534716+fortis852@users.noreply.github.com>
2025-08-08 10:19:34 +0300

    Create CameraService.java

---
+++ src/main/java/com/cleanhdmi/CameraService.java
@@ -0,0 +1,33 @@
+package com.cleanhdmi;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class CameraService extends Service {
+    private static final String TAG = "CameraService";
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "Service created");
+    }
+    
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "Service started");
+        return START_STICKY;
+    }
+    
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Service destroyed");
+    }
+}
